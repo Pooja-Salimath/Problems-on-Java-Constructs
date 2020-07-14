627aef6a1936cc351b7af8647f52d0ac13f42874
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-14 23:44:12 +0530

    Added Part Time Employee and Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,6 +4,7 @@ class EmployeeWageComputation
 	{
 		System.out.println("-----: Welcome to Employee Wage Computaion Program :-----");
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
 		int EMP_RATE_PER_HOUR = 20;
 
 		int empHrs = 0;
@@ -11,16 +12,12 @@ class EmployeeWageComputation
 
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == IS_FULL_TIME)
-		{
 			empHrs = 8;
-			empWage = EMP_RATE_PER_HOUR * empHrs;
-			System.out.println("Employee is Preasent");
-			System.out.println("Employee Wage is:" +empWage);
-		}
 		else
-		{
-			System.out.println("Employee is Abscent");	
-		}
+			empHrs = 4;
+			empWage = EMP_RATE_PER_HOUR * empHrs;
+			System.out.println("Employee Wage is:" +empWage);
+
 	}
 
 }
