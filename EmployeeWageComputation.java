c9319b7079e7b6de63bf17959eb7718e1aee46cc
Pooja Virupakshi Salimath <20911@emudhra.local>
2020-07-15 00:42:27 +0530

    Calculating Wages For Total Working Hours and Days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,15 +4,17 @@ public class EmployeeWageComputation
         public static final int IS_PART_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAY = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
 
 	public static void main(String[] args)
 	{
 		System.out.println("-----: Welcome to Employee Wage Computaion Program :-----");
 
-		int empHrs = 0,empWage = 0,totalWage = 0;
+		int empHrs = 0,empWage = 0,totalWage = 0,totalEmpHrs = 0,totalWorkingDays = 0;
 
-		for ( int day = 0; day < NUM_OF_WORKING_DAY; day++)
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAY)
 		{
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck)
 			{
@@ -25,10 +27,10 @@ public class EmployeeWageComputation
 				default:
 			      empHrs = 0;
 			}
-			empWage = EMP_RATE_PER_HOUR * empHrs;
-			totalWage += empWage;
-			System.out.println("Employee Wage is:" +empWage);
+			totalEmpHrs += empHrs;
+			System.out.println("Day#:" +totalWorkingDays+ "  Emp Hrs:" +empHrs);
 	   	}
-		System.out.println("Employee Total Wage is:" +totalWage);
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Total Wage is:" +totalEmpWage);
 	}
 }
